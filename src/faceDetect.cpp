526f2ba13f59d1a94e84e9d12f214b1af465339b
iaakhter <iaakhter@colby.edu>
2016-05-06 10:45:49 -0400

    Added npr stuff and comments

--- src/faceDetect.cpp
+++ src/faceDetect.cpp
@@ -1,12 +1,15 @@
-/* Itrat Ahmed Akhter
- * CS 365
- * Project 5
- * idBagVideoHaar.cpp
- * April 19, 2016
- * Program that uses the HAAR cascade classifier to detect
- * Wendy the badger in a live stream video 
+/* Itrat Akhter and Beatrice Lang
+ * Two windows will be seen when this program is run.
+ * One window will show the face on the video swapped with
+ * a face from an image. This swapping is rotation invariant
+ * as long as the faces are detected in the video and the image
+ * and the eyes are detected in the video. Rotation invariance
+ * is implemented using the eye locations. 
+ * The other window will show teardrops running down the face
+ * when the face is detected. Otherwise, we will see an oil painted
+ * rendering of the video. Teardrops are implemented using a version
+ * of particle system.
  */
-
 
 #include <cstdio>
 #include <cstring>
@@ -15,15 +18,19 @@
 #include <math.h>
 #include "opencv2/opencv.hpp"
 
+//For tears
  typedef struct Tears{
- 	int x;
- 	int y;
- 	int state;
+ 	int x; //x position
+ 	int y; //y position
+ 	int state; //state = 0 if tear is falling and 1 otherwise
  }Tear;
 
+ /* Replace face in frame with face in swapping. objects and swapObjects
+  * hold bounding boxes around faces in frame and swapping respectively.
+  * theta is the angle made by line between the eyes with the x axis
+  */
  void replaceFace(cv::Mat frame, cv::Mat swapping, std::vector<cv::Rect> objects, std::vector<cv::Rect> swapObjects, float theta){
  	float thetaDegrees = (theta)*(180/M_PI);
- 	//printf("theta %f\n",thetaDegrees);
  	int i,j;
  	//replace the face in the image with the face in the video
 	cv::Mat replacingFace;
@@ -85,15 +92,25 @@
 	else{
 		eyeVectory = centerEye2y - centerEye1y;
 	}
-	//printf("eyeVectorx %f eyeVectory %f\n",eyeVectorx,eyeVectory);
 	float theta = atan2(eyeVectory,eyeVectorx);
 
-	rectangle(frame,cv::Point(eye1x,eye1y),cv::Point(eye1x+eye1Width,eye1y+eye1Height),cv::Scalar(0,0,255));
-	rectangle(frame,cv::Point(eye2x,eye2y),cv::Point(eye2x+eye2Width,eye2y+eye2Height),cv::Scalar(0,0,255));
+	//rectangle(frame,cv::Point(eye1x,eye1y),cv::Point(eye1x+eye1Width,eye1y+eye1Height),cv::Scalar(0,0,255));
+	//rectangle(frame,cv::Point(eye2x,eye2y),cv::Point(eye2x+eye2Width,eye2y+eye2Height),cv::Scalar(0,0,255));
 	//line(frame,cv::Point(centerEye1x,centerEye1y),cv::Point(centerEye2x,centerEye2y),cv::Scalar(0,0,255));
  	return theta;
  }
 
+ /* Adds tears to the video. The parameters are as follows:
+  * frame - the video frame
+  * teardrop - the image of the teardrop
+  * eyeObjects - the bounding boxes of eyes detected
+  * tears - array of tear objects
+  * numberOftears - number of tears
+  * frameNumber - total number of frames from the time that eyes are detected
+  * faceHeight - height of the face
+  * differences - array to hold the differences between the current y position of
+  				 tears and the eye
+  */
  void addTeardrop(cv::Mat frame, cv::Mat teardrop, std::vector<cv::Rect> eyeObjects,
  				 Tear *tears, int numberOfTears,int frameNumber, int faceHeight, int* differences){
  	int i,j;
@@ -105,8 +122,10 @@
  	int tWidth = teardrop.size().width;
  	int tHeight = teardrop.size().height;
 
+ 	//speed of the tear
  	int speed = rand()%tHeight+10;
 
+ 	//modify teardrop image
  	cv::Mat mask = cv::Mat::zeros(tHeight,tWidth,CV_8UC1);
  	for(i = 0; i<tHeight; i++){
  		for(j = 0; j<tWidth; j++){
@@ -125,6 +144,11 @@
  	x2 = eyeObjects[1].x+eyeObjects[1].width/2;
  	y2 = eyeObjects[1].y+eyeObjects[1].height/1.5;
  	srand(time(NULL));
+
+ 	//Initialize tear fields, states and differences when
+ 	//eyes are first detected, that is when frameNumber = 0;
+ 	//Assign half of the tears to the left eye and the other half
+ 	//to the right eye
  	if(frameNumber==0){
  		for(i = 0; i<numberOfTears; i++){
  			if(i>=numberOfTears/2){
@@ -138,9 +162,15 @@
  			tears[i].state = 0;
  			differences[i] = 0;
  		}
- 		//printf("done with frame 0\n");
  	}
+
+ 	//When the frame number is greater than 0, make the
+ 	//tears fall
  	else{
+ 		//Check states of tears again. If the states
+ 		//are zero then bring the tears back to the eyes
+ 		//Change the state of one tear from each eye from
+ 		//0 to 1 to make one tear fall from each eye
  		numberOf1s = 0;
  		for(i = 0; i<numberOfTears/2; i++){
  			if(tears[i].state==0){
@@ -164,6 +194,16 @@
  			}
  		}
  	}
+
+ 	//Change the position of the tears according to the states of
+ 	//the tears. If the state is 1, update the differences value
+ 	//and then update the position of the tear by adding the difference
+ 	//value to the current position of the eye.
+ 	//The reason we do not update the position of the tears directly
+ 	//with the speed, is because the position of the eyes might change
+ 	//and we want to be able to adjust the position of tears accordingly
+ 	//(with the position of the eyes);
+ 	//When the tears reach the end of face, change state to 0 and the positions
  	for(i = 0; i<numberOfTears; i++){
  		if(tears[i].state==1){
 			differences[i]+=speed;
@@ -188,13 +228,80 @@
 			}
 		}
  	}
- 
+ 	
+ 	//finally draw the tears
  	for(i = 0; i<numberOfTears; i++){
  		//printf("i %d x %d y %d \n",i,tears[i].x,tears[i].y);
  		teardrop.copyTo(frame(cv::Rect(tears[i].x,tears[i].y,tWidth,tHeight)),mask);
  	}
  }
 
+//creates an oil painting effect on the frame 
+//algotithm taken from https://softwarebydefault.com/
+void oilPaint(cv::Mat frame){
+	cv::Mat gray;
+	int x,y,i,j,k;
+	int maxIntensity,intensity,maxIndex;
+	int temp;
+	int filter = 1;
+	int levels = 30;
+	int red[levels+1];
+	int green[levels+1];
+	int blue[levels+1];
+	int intensityLevel[levels+1];
+	cv::Mat copy(frame);
+	cv::cvtColor(frame,gray,CV_BGR2GRAY);
+	for(x = 0; x<frame.size().width;x++){
+		for(y = 0; y<frame.size().height;y++){
+			gray.at<uchar>(y,x) = gray.at<uchar>(y,x)%levels;
+		}
+	}
+
+	for(x = 0; x<frame.size().width; x++){
+		for(y = 0; y<frame.size().height; y++){
+			maxIntensity = 0;
+			maxIndex = 0;
+			for(k =0; k<=levels; k++){
+				red[k] = 0;
+				green[k] = 0;
+				blue[k] = 0;
+				intensityLevel[k] = 0;
+			}
+			for(i = x-filter; i<=x+filter; i++){
+				for(k = y-filter; k<=y+filter; k++){
+					if(i>=0 && i< frame.size().width && k>=0 && k<frame.size().height){
+						cv::Vec3b color = copy.at<cv::Vec3b>(cv::Point(i,k));
+						intensity = gray.at<uchar>(k,i);
+						intensityLevel[intensity] += 1;
+						red[intensity]+=color[0];
+						green[intensity]+=color[1];
+						blue[intensity]+=color[2];
+						if(intensityLevel[intensity]>maxIntensity){
+							maxIntensity = intensityLevel[intensity];
+							maxIndex = intensity;
+						}
+					}
+				}
+			}
+			cv::Vec3b newColor;
+			newColor[0] = red[maxIndex]/maxIntensity;
+			newColor[1] = green[maxIndex]/maxIntensity;
+			newColor[2] = blue [maxIndex]/maxIntensity;
+			frame.at<cv::Vec3b>(cv::Point(x,y)) = newColor;
+
+		}
+		//blur some more
+		int offsetx = rand()%20 - 10;
+		int offsety = rand()%20 - 10;
+		int curx = x+offsetx;
+		int cury = y+offsety;
+		if(curx >=0 && curx<frame.size().width && cury>=0 && cury<frame.size().height){
+			frame.at<cv::Vec3b>(cv::Point(curx,cury)) = copy.at<cv::Vec3b>(cv::Point(x,y));
+
+		}
+	}
+
+}
 
 /*Main function*/
 int main(int argc, char *argv[]) {
@@ -243,14 +350,16 @@ int main(int argc, char *argv[]) {
 
 	cv::namedWindow("Video", 1);
 	int frameNumber = 0;
-
+	int oilP = 0;
 	for(;;) {
+		oilP = 0;
 		char keyPressed = cv::waitKey(10);
 
 		cv::Mat frame,frame2;
 		
 		*capdev >> frame; // get a new frame from the camera, treat as a stream
 		*capdev >> frame2;
+
 		resize(frame, frame, cv::Size(640, 360), 0, 0, cv::INTER_CUBIC);
 		resize(frame2, frame2, cv::Size(640, 360), 0, 0, cv::INTER_CUBIC);
 
@@ -258,26 +367,50 @@ int main(int argc, char *argv[]) {
 		cascade.detectMultiScale(frame,objects,1.1,1,0,cv::Size(100,100),cv::Size(frame.size().width/2,frame.size().height/2));
 		//detect eyes in the video
 		eyeCascade.detectMultiScale(frame,eyeObjects,1.1,1,0,cv::Size(30,30),cv::Size(frame.size().width/2,frame.size().height/2));
+		
 		float theta =0;
 		if(objects.size()==1){
 			//printf("found face\n");
-			rectangle(frame2,cv::Point(objects[0].x,objects[0].y),cv::Point(objects[0].x+objects[0].width,objects[0].y+objects[0].height),cv::Scalar(0,0,255));
+			//rectangle(frame2,cv::Point(objects[0].x,objects[0].y),cv::Point(objects[0].x+objects[0].width,objects[0].y+objects[0].height),cv::Scalar(0,0,255));
 		}
+
+		//If face is not detected or face is too far away then
+		//show an oil painting rendering of the video
+		if(objects.size()==0 || objects[0].width/(float)frame.size().width<0.2){
+			printf("Face far away or no face detected\n");
+			oilPaint(frame2);
+			oilP = 1;
+		}
+
 
 		//find angle of face with the help of eyes
 		if(eyeObjects.size()==2){
-			printf("found eyes\n");
+			//printf("found eyes\n");
 			theta = findAngle(frame2,eyeObjects);
-			addTeardrop(frame2,teardrop,eyeObjects,tears,numberOfTears,frameNumber,objects[0].y+objects[0].height,differences);
+			//also if you can detect eyes, and your video is not oilPainted
+			//cry because you are perpetually sad
+			if(oilP==0){
+				addTeardrop(frame2,teardrop,eyeObjects,tears,numberOfTears,frameNumber,objects[0].y+objects[0].height,differences);
+			}
 			frameNumber++;
 		}
+		//When eye not detected change frameNumber to 0, so all 
+		//tears have state 0 (the position of all the tears will now
+		//be at the eye again)
+		else{
+			frameNumber = 0;
+		}
+
+		//show frame2 with oilpainting/teardrop in a separate window
+		cv::imshow("MyFace", frame2);
 
 		//replace face in video with face in image
-		cv::imshow("MyFace", frame2);
+		//show swapped face in a aseparate window
 		if(objects.size()==1 && swapObjects.size()==1){
 			replaceFace(frame,swapping,objects,swapObjects,theta);
 		}
-		//cv::imshow("Beyonce", frame);
+		cv::imshow("Beyonce", frame);
+	
 
 		//press q to quit
 		if(keyPressed==113)
